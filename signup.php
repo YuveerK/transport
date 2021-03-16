bc1b7441216d8d6e2da710b1fba7998c51205401
YuveerK <yuveerkal01@gmail.com>
2021-03-16 12:30:25 +0200

    first commit

---
+++ signup.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+
+    <div class="nav_container">
+        <div class="logo">
+            <h1>A&H Transport</h1>
+        </div>
+
+        <div class="links">
+            <ul>
+                <li>
+                    <a href="#">Login</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">Sign up</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+
+    <div class="content_container">
+        <form action="signup.php" method="post"></form>
+
+
+    </div>
+
+
+</body>
+
+</html>
